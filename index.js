f48d5eb24b376eb9b8c0950f50f10aea29b249a6
cpan1995 <cpan1995@gmail.com>
2021-06-25 20:50:48 -0400

    commit tree 0208adb42d7d6bb4cb71b87f312571255f13774b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+var currentUser = "Cheng Pan"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = `Welcome, ${firstInitial}!` 
